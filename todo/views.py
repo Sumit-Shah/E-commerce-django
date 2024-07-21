71bc9817d100e7ba2cdd41fc9b75233c06da4100
Sumit-Shah <sumitshah001122@gmail.com>
2024-07-21 07:26:22 +0545

    Learning From Template

---
+++ todo/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    people=[
+        {
+            'name':"Sumit",
+            'age':25,
+            'gender':'Male'
+        },
+        {
+            'name':"Santosh",
+            'age':22,
+            'gender':'Male'
+        },
+        {
+            'name':"Rajni",
+            'age':26,
+            'gender':'Female'
+        }
+    ]
+    
+    context={
+        'people':people
+    }
+    return render(request, 'index.html',context)
+
+def about_us(request):
+    return HttpResponse("<h1>About Us</h1>")
