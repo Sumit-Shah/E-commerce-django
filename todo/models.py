624541f8bc3040cc9cd6e6edd210350cb053d1f1
Sumit-Shah <sumitshah001122@gmail.com>
2024-07-26 17:27:44 +0545

    sucessfully inserted the data

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+"""
+todo
+    -name (string)
+    -description (string)
+    -status (boolean)
+"""
+
+
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description=models.TextField()
+    status=models.BooleanField(default=False)
